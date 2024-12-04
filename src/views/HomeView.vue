56e29062a9d5b36558b256a42427180ae2b19056
Netokingpo <ernestozb22@gmail.com>
2024-12-04 12:54:02 -0700

    Building

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="w-screen h-screen flex justify-center items-center bg-neutral-950">
-    <p class="font-mono text-3xl text-neutral-100">Hola mundo...</p>
+  <div class="w-screen h-screen flex justify-center items-center bg-[#fff9f2]">
+    <img src="../assets/img/Building.png" class="md:w-1/6">
   </div>
 </template>
